9a5aa549a4df0d2fca8049a490b94b577bc98daf
opensource <opensource@foodrev.org>
2022-06-06 05:00:38 +0200

    Config to 10MHz and higher density

--- config.tcl
+++ config.tcl
@@ -24,7 +24,7 @@ set ::env(VERILOG_FILES) "$::env(DESIGN_DIR)/wrapper.v \
     $::env(DESIGN_DIR)/wavelet_transform/src/output_multiplexer.v"
 
 # target density, change this if you can't get your design to fit
-set ::env(PL_TARGET_DENSITY) 0.50
+set ::env(PL_TARGET_DENSITY) 0.7
 
 # don't put clock buffers on the outputs, need tristates to be the final cells
 set ::env(PL_RESIZER_BUFFER_OUTPUT_PORTS) 0
@@ -37,7 +37,7 @@ set ::env(FP_SIZING) absolute
 set ::env(SYNTH_DEFINES) "MPRJ_IO_PADS=38"
 
 # clock period is ns
-set ::env(CLOCK_PERIOD) "200"
+set ::env(CLOCK_PERIOD) "100"
 set ::env(CLOCK_PORT) "wb_clk_i"
 
 # macro needs to work inside Caravel, so can't be core and can't use metal 5
